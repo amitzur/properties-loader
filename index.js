a2ea5b4140f82b727bd7de711e4755a1f031da6b
Amit Zur <sendwithchibo@gmail.com>
2017-08-01 23:51:47 +0300

    Update index.js
    webpack 2 compatibility

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 module.exports = function(content) {
   var callback = this.async();
 
-  var query = require('querystring').parse(this.query.replace(/^\?/, ''));
-  query.path = false;
+  const options = loaderUtils.getOptions(this);
 
-  for (var k in query) if (query.hasOwnProperty(k)) {
-    if (/(^\[|^(true|false)$)/.test(query[k])) {
-      query[k] = JSON.parse(query[k]);
-    }
-  }
-
-  require('properties').parse(content, query, function(err, obj) {
+  require('properties').parse(content, options, function(err, obj) {
     if (err) { return console.error(err); }
     callback(null, 'module.exports = ' + JSON.stringify(obj) + ';');
   });
